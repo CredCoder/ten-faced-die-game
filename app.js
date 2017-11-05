3ea0c55b380f13573f53d4db7f042a24d000d5a4
Doug Ross <credcoder@gmail.com>
2017-11-05 21:57:05 +0000

    add state variable instead of display "none" add images/directory to image src so image src="images/die-" + die + ".png" in javascript and html updated as well, would not work the way it was

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
-
 var activePlayer = 0;
-var scores = document.querySelectorAll(".score-player");
-var currents = document.querySelectorAll(".score-current-player");
 var die;
+var gamePlaying = true;
 var current = document.querySelector("#current-" + activePlayer);
 var score = document.querySelector("#score-" + activePlayer);
 var dieDisplay = document.querySelector(".die");
 var rollBtn = document.querySelector(".btn-roll");
-var holdBtn = document.querySelector(".btn-hold");
+var saveBtn = document.querySelector(".btn-hold");
 var panel = document.querySelector(".player-" + activePlayer + "-panel");
 var resetBtn = document.querySelector(".btn-new");
 
@@ -16,13 +13,13 @@ dieDisplay.style.display = 'none';
 
 resetBtn.addEventListener("click", function(){
    dieDisplay.style.display = 'none';
-   rollBtn.style.display = "block";
-   scores[0].textContent = 0;
-   scores[1].textContent = 0;
-   currents[0].textContent = 0;
-   currents[1].textContent = 0;
+   gamePlaying = true;
+   document.querySelectorAll(".score-player")[0].textContent = 0;
+   document.querySelectorAll(".score-player")[1].textContent = 0;
+   document.querySelectorAll(".score-current-player")[0].textContent = 0;
+   document.querySelectorAll(".score-current-player")[1].textContent = 0;
    if (activePlayer === 1)
-   rollBtn.innerHTML = "<i class=" + "ion-ios-loop" + "></i>" + "Roll"
+   rollBtn.innerHTML = "<i class=" + "ion-ios-loop" + "></i>" + "Roll";
    score.style.color = "rgb(20, 20, 100)";
    panel.classList.remove("active");
    activePlayer = 0;
@@ -32,51 +29,53 @@ resetBtn.addEventListener("click", function(){
    panel.classList.add("active");
 });
 
-holdBtn.addEventListener("click", function(){
+saveBtn.addEventListener("click", function(){
+   if (score.textContent === "Winner") {
+      score.textContent = "Winner";
+   } else {
    score.textContent = Number(current.textContent) + Number(score.textContent);
    current.textContent = 0;
    toggle();
+   }
 });
 
 rollBtn.addEventListener("click", function(){
-   roll();
-   if (die === 0) {
-      current.textContent = 0;
-      toggle();
-   } else {
-      current.textContent = Number(current.textContent) + die;   
-   }
-   if (Number(current.textContent) + Number(score.textContent) === 100) {
-      score.textContent = "Winner";
-      score.style.color = "red";
-      rollBtn.style.display = "none";
-   } else if (Number(current.textContent) + Number(score.textContent) > 100) {
+   if (gamePlaying) {
+      roll();
+      if (die === 0) {
+         current.textContent = 0;
+         toggle();
+      } else {
+         current.textContent = Number(current.textContent) + die;   
+      }
+   } if (Number(current.textContent) + Number(score.textContent) > 100) {
       score.textContent = (Number(current.textContent) + Number(score.textContent)) - die;
       current.textContent = 0;
       toggle();
+   } else if (Number(current.textContent) + Number(score.textContent) === 100) {
+      score.textContent = "Winner";
+      score.style.color = "red";
+      gamePlaying = false;
    }
 });
-
-
-
 
 function roll() {
    die = Math.floor(Math.random()*10);
    dieDisplay.style.display = 'block';
-   dieDisplay.src = "die-" + die  + ".png";
+   dieDisplay.src = "images/die-" + die  + ".png";
 }
 
 function toggle() {
    if (activePlayer === 0) {
       panel.classList.remove("active");
-      rollBtn.innerHTML = "<i class=" + "ion-ios-loop" + "></i>" + "SHOOT"
+      rollBtn.innerHTML = "<i class=" + "ion-ios-loop" + "></i>" + "SHOOT";
       activePlayer = 1;
       panel = document.querySelector(".player-" + activePlayer + "-panel");
       panel.classList.add("active");
    } else {
       panel.classList.remove("active");
       activePlayer = 0;
-      rollBtn.innerHTML = "<i class=" + "ion-ios-loop" + "></i>" + "Roll"
+      rollBtn.innerHTML = "<i class=" + "ion-ios-loop" + "></i>" + "Roll";
       panel = document.querySelector(".player-" + activePlayer + "-panel");
       panel.classList.add("active");
    }
